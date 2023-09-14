8da628daf9c750c11b5813e0491a68c71c636064
Zuhayr Amin <44747492+zuhayramin@users.noreply.github.com>
2023-09-14 19:49:07 +0400

    finish repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
+const repeatString = function (string, num) {
+    let repeatedString = ""
+    if (num < 0) {
+        return "ERROR"
+    }
+    for (let i = 0; i < num; i++) {
+        repeatedString += string
+    }
+    // return repeatedString
+    return repeatedString
+}
 
 // Do not edit below this line
-module.exports = repeatString;
+module.exports = repeatString
