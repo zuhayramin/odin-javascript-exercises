4aa5fe7915e7a00fd4dd3be2ed99741f21bab284
Zuhayr Amin <44747492+zuhayramin@users.noreply.github.com>
2023-09-14 22:31:28 +0400

    sumAll function rejects null and non-number values

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    let sum = 0
+    let smallerNumber
+    let largerNumber
 
-};
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR"
+    }
+
+    if (typeof num1 === "number" && typeof num2 === "number") {
+        if (num1 < num2) {
+            smallerNumber = num1
+            largerNumber = num2
+        } else if (num1 > num2) {
+            smallerNumber = num2
+            largerNumber = num1
+        }
+
+        for (let i = smallerNumber; i <= largerNumber; i++) {
+            sum += i
+        }
+        return sum
+    } else return "ERROR"
+}
 
 // Do not edit below this line
-module.exports = sumAll;
+module.exports = sumAll
