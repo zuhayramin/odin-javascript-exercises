f52a1e6d2133e1d6b93abf12a8eb9dc29fb35e75
Zuhayr Amin <44747492+zuhayramin@users.noreply.github.com>
2023-09-14 22:38:23 +0400

    fix leapYears function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,8 @@
-const leapYears = function() {
-
-};
+const leapYears = function (year) {
+    if ((year % 4 === 0 && year % 100 != 0) || year % 400 === 0) {
+        return true
+    } else return false
+}
 
 // Do not edit below this line
-module.exports = leapYears;
+module.exports = leapYears
