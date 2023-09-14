28be07a84b973e2b6edec4a04e059d6677835cbe
Zuhayr Amin <44747492+zuhayramin@users.noreply.github.com>
2023-09-14 22:45:53 +0400

    fixed decimal point of results

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function (temp) {
+    let celsiusTemp = (temp - 32) / (9 / 5)
+    return Math.round(celsiusTemp * 10, 1) / 10
+}
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function (temp) {
+    let fahrenheitTemp = temp * (9 / 5) + 32
+    return Math.round(fahrenheitTemp * 10, 1) / 10
+}
+
+console.log(convertToFahrenheit(73.2))
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
-};
+    convertToCelsius,
+    convertToFahrenheit,
+}
