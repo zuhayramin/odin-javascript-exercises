28db2dcd28c33e77858a7a56884f2fba6225675f
Zuhayr Amin <44747492+zuhayramin@users.noreply.github.com>
2023-09-14 19:59:33 +0400

    fix reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
-};
-
+const reverseString = function (word) {
+    let reversedWord = ""
+    for (let i = word.length - 1; i > -1; i--) {
+        reversedWord += word[i]
+    }
+    return reversedWord
+}
 // Do not edit below this line
-module.exports = reverseString;
+module.exports = reverseString
