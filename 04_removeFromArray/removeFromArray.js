97508ad65e2377117141a79d5ebbe68fe58d0518
Zuhayr Amin <44747492+zuhayramin@users.noreply.github.com>
2023-09-14 22:17:00 +0400

    Finish removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+    let list = arguments[0]
+    for (let i = 1; i < arguments.length; i++) {
+        let exclusionIndex = list.indexOf(arguments[i])
+        if (list[exclusionIndex] === arguments[i]) {
+            list.splice(exclusionIndex, 1)
+        }
+    }
+    return list
+}
 
-};
-
+console.log(removeFromArray([1, 2, 3, 4], "3", 4))
 // Do not edit below this line
-module.exports = removeFromArray;
+module.exports = removeFromArray
