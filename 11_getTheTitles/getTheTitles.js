aaf93d65a9ada856b1e682c21bbd9bf65d7a254e
Zuhayr Amin <44747492+zuhayramin@users.noreply.github.com>
2023-10-03 10:15:53 +0400

    complete getTheTitle exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,6 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function (books) {
+    return books.map((book) => book.title)
+}
 
 // Do not edit below this line
-module.exports = getTheTitles;
+module.exports = getTheTitles
