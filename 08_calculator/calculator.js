91e949de701728ab6c87c08f057692da4e22774d
Zuhayr Amin <44747492+zuhayramin@users.noreply.github.com>
2023-10-03 09:52:32 +0400

    Practice built-in functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,37 @@
-const add = function() {
-	
-};
+const add = function (a, b) {
+    return a + b
+}
 
-const subtract = function() {
-	
-};
+const subtract = function (a, b) {
+    return a - b
+}
 
-const sum = function() {
-	
-};
+const sum = function (numbers) {
+    return numbers.reduce((total, num) => total + num, 0)
+}
 
-const multiply = function() {
+const multiply = function (numbers) {
+    return numbers.reduce((total, num) => total * num, 1)
+}
 
-};
+const power = function (a, b) {
+    return Math.pow(a, b)
+}
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
-
+const factorial = function (number) {
+    if (number === 0) return 1
+    let total = 1
+    for (let index = 1; index <= number; index++) {
+        total *= index
+    }
+    return total
+}
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
-};
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial,
+}
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,7 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function (str) {
+    str = str.toLowerCase().replace(/[^a-z0-9]/g, "")
+    let temp = str.split("").reverse().join("")
+    return temp === str ? true : false
+}
 // Do not edit below this line
-module.exports = palindromes;
+module.exports = palindromes
