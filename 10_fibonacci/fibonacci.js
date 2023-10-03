e510648f58a531449fd3c3d67853f43143f4620f
Zuhayr Amin <44747492+zuhayramin@users.noreply.github.com>
2023-10-03 10:13:17 +0400

    complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    num = parseInt(num)
+    if (num <= 0) return "OOPS"
+    let fibonacci = [1, 1, 2, 3, 5]
 
-};
+    if (num > 4) {
+        for (let index = 4; index < num; index++) {
+            fibonacci.push(fibonacci[index] + fibonacci[index - 1])
+        }
+        return fibonacci[num - 1]
+    } else return fibonacci[num - 1]
+}
 
+console.log(fibonacci(6))
 // Do not edit below this line
-module.exports = fibonacci;
+module.exports = fibonacci
